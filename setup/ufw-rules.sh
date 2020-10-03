16d4ebee9bcad8fc0c6769da0e8b3801b141d0d0
Nathan Files <nathanwfiles@gmail.com>
2020-10-03 16:53:45 -0400

    allows mosh service

--- setup/ufw-rules.sh
+++ setup/ufw-rules.sh
@@ -3,13 +3,14 @@
 set -e
 set -o pipefail
 
-# tailscale port
+# tailscale
 ufw allow 41641/udp
 
 # services
 ufw allow in on tailscale0 to any port 22
 ufw allow in on tailscale0 to any port 80
 ufw allow in on tailscale0 to any port 53
+ufw allow in on tailscale0 to any port 60000:60100 proto udp
 
 # defaults
 ufw default deny incoming
