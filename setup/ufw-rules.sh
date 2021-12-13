d21d751e85480c64a4e6b8e9f4a8a7c25ba5df08
Nathan Files <nathanwfiles@gmail.com>
2021-12-13 14:38:07 -0500

    adds home assistant

--- setup/ufw-rules.sh
+++ setup/ufw-rules.sh
@@ -10,6 +10,7 @@ ufw allow 41641/udp
 ufw allow in on tailscale0 to any port 22
 ufw allow in on tailscale0 to any port 80
 ufw allow in on tailscale0 to any port 53
+ufw allow in on tailscale0 to any port 8123
 ufw allow in on tailscale0 to any port 60000:60100 proto udp
 
 # defaults
