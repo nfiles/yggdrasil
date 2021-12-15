812e30c00bf6ad624082f97a938dc69dc5588798
Nathan Files <nathanwfiles@gmail.com>
2021-12-14 20:43:06 -0500

    enables remote access with caddy reverse proxy

--- setup/ufw-rules.sh
+++ setup/ufw-rules.sh
@@ -5,15 +5,24 @@ set -o pipefail
 
 # tailscale
 ufw allow 41641/udp
+# 80 is automatically redirected to 443 on the .dev TLD
+ufw allow 443
 
 # services
+# ssh
 ufw allow in on tailscale0 to any port 22
+# http/https
 ufw allow in on tailscale0 to any port 80
+ufw allow in on tailscale0 to any port 443
+# pihole web UI
+ufw allow in on tailscale0 to any port 8080
+# pihole DNS
 ufw allow in on tailscale0 to any port 53
+# Home Assistant
 ufw allow in on tailscale0 to any port 8123
+# mosh
 ufw allow in on tailscale0 to any port 60000:60100 proto udp
 
 # defaults
 ufw default deny incoming
 ufw default allow outgoing
-
